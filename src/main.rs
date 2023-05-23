4e884a762d87c541a391cf83808f3c40d705eba9
Martin Horčička <martin.horcicka@ingenico.com>
2023-05-23 12:30:28 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-#[macro_use]
-extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, World!"
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn main() {
+    println!("Hello, world!");
 }
