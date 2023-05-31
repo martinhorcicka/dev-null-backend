b5d131fd8111d0178839811c6e28cdd76aa7f399
Martin Horčička <martin.horcicka@ingenico.com>
2023-05-31 15:49:30 +0200

    permissive CORS layer

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use axum::{
 use report::mc::packet::{ping::PingResponse, status::StatusResponse};
 use response::Response;
 use send_email::SendEmailData;
+use tower_http::cors::CorsLayer;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
@@ -28,7 +29,8 @@ async fn main() {
     let app = Router::new()
         .route("/send_email", post(send_email))
         .route("/report/mc/ping/:payload", get(mc_server_ping))
-        .route("/report/mc/status", get(mc_server_status));
+        .route("/report/mc/status", get(mc_server_status))
+        .layer(CorsLayer::permissive());
 
     let ip = std::env::var("BACKEND_ADDR").expect("cannot run without specified address");
     let port = std::env::var("BACKEND_PORT").expect("cannot run without specified port");
